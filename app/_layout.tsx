80277af96b3c41f8af3884f272962c19820db599
cirengspicy <cirengspicy@gmail.com>
2025-05-22 15:12:06 +0700

    change in layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
 import { useFonts } from 'expo-font';
- 
+
 export default function Layout() {
   const [fontsLoaded] = useFonts({
     'Codec-Cold-Light': require('../assets/fonts/Codec-Cold-Light-trial.ttf'),
